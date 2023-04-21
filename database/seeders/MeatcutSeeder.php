3c4be769583a7148b6936e5229df4f13e4477c33
eddieluis37 <eddierada@gmail.com>
2023-04-21 07:51:52 -0500

    Se complementan los 28 registros de cortes de Res

--- database/seeders/MeatcutSeeder.php
+++ database/seeders/MeatcutSeeder.php
@@ -16,9 +16,199 @@ class MeatcutSeeder extends Seeder
     {
         Meatcut::create([
         	'category_id' => 1,
-        	'name' => 'Pacha',
+        	'name' => 'PACHAS',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'CADERA COMPLETA',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'BOLA DE PIERNA',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'CHATAS COMPLETA',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'LOMO ENTERO',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+		Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'BRAZO COMPLETO',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'DESCARGUE COMPLETO',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+		Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'PECHO CON TAPA',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'MURILLO',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+		Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'COGOTE',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+		Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'COSTILLA DELANTERA',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'COSTILLA COMPLETA',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'COLA ENTERA',
+        	'description' => '',
+        	'status' => true
+        ]);
+		
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'HUESO DE PALETA',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'TOMAHAWK-TBONE',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'HUESO ESPECIAL',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'HUESO AGUJA',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'HUESO CARNUDO',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'HUESO DE PECHO',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'VIRILIS',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'CREADILLA',
+        	'description' => '',
+        	'status' => true
+        ]);
+        
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'SEBO PECHO',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'SEBO',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'HUESO POROSO',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'MOTA',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'CHOCOZUELA',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'RILA',
+        	'description' => '',
+        	'status' => true
+        ]);
+
+        Meatcut::create([
+        	'category_id' => 1,
+        	'name' => 'UBRE',
         	'description' => '',
         	'status' => true
         ]);
     }
+    
 }
