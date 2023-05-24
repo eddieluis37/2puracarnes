caf7c5c0fedf901c86259661af7f4cb4b423f28f
system-laboratorio <system-laboratorioq@gmail.com>
2023-05-23 23:50:43 -0600

    added trigger

---
+++ database/migrations/2023_05_24_001333_create_trigger_update_precio_compra.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateTriggerUpdatePrecioCompra extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('
+            CREATE TRIGGER trigger_update_precio_compra AFTER INSERT ON compensadores_details FOR EACH ROW
+            BEGIN
+                UPDATE products
+                SET cost = NEW.pcompra
+                WHERE id = NEW.products_id;
+            END
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP TRIGGER IF EXISTS trigger_update_precio_compra');
+    }
+}
