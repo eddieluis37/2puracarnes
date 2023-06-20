a0c3ac8472d3fedd22ce35f56644f0c29bd2eebd
eddieluis37 <eddieluis37@users.noreply.github.com>
2023-06-20 17:01:13 -0500

    Refactoriza para eliminar redundancia en la funcion. Only one of them should be used. - The  type  property in the  ajax  object is unnecessary as "GET" is the default value. - The  language  object can be simplified by removing the properties that have default values. - The  select2  initialization code can be refactored to reduce code duplication.

--- public/rogercode/js/res/beneficiores/rogercode-beneficiores-index.js
+++ public/rogercode/js/res/beneficiores/rogercode-beneficiores-index.js
@@ -1,70 +1,57 @@
 console.log("benedicore Starting");
-$(document).ready(function () {
-    $(function () {
-        $("#tableBeneficiores").DataTable({
-            paging: true,
-            pageLength: 5,
-            /*"lengthChange": false,*/
-            autoWidth: false,
-            processing: true,
-            serverSide: true,
-            ajax: {
-                url: "/showbeneficiores",
-                type: "GET",
-            },
-            columns: [
-                { data: "id", name: "id" },
-                { data: "namethird", name: "namethird" },
-                { data: "date", name: "date" },
-                { data: "factura", name: "factura" },
-                { data: "lote", name: "lote" },
-                { data: "action", name: "action" },
-            ],
-            order: [[0, 'DESC']],
-            language: {
-                processing: "Procesando...",
-                lengthMenu: "Mostrar _MENU_ registros",
-                zeroRecords: "No se encontraron resultados",
-                emptyTable: "Ningún dato disponible en esta tabla",
-                sInfo: "Mostrando del _START_ al _END_ de total _TOTAL_ registros",
-                infoEmpty:
-                    "Mostrando registros del 0 al 0 de un total de 0 registros",
-                infoFiltered: "(filtrado de un total de _MAX_ registros)",
-                search: "Buscar:",
-                infoThousands: ",",
-                loadingRecords: "Cargando...",
-                paginate: {
-                    first: "Primero",
-                    last: "Último",
-                    next: "Siguiente",
-                    previous: "Anterior",
-                },
-            },
-        });
-    });
-    $(".selectProvider").select2({
-        placeholder: "Busca un proveedor",
-        width: "100%",
-        theme: "bootstrap-5",
-        allowClear: true,
-        dropdownParent: $("#modal-create-beneficiore"),
-        //https://select2.org/troubleshooting/common-problems
-    });
-    $(".selectPieles").select2({
-        placeholder: "Buscar un Cliente Piel",
-        width: "100%",
-        theme: "bootstrap-5",
-        allowClear: true,
-        dropdownParent: $("#modal-create-beneficiore"),
-    });
-    $(".selectVisceras").select2({
-        placeholder: "Buscar un Cliente Viscera",
-        width: "100%",
-        theme: "bootstrap-5",
-        allowClear: true,
-        dropdownParent: $("#modal-create-beneficiore"),
-    });
+$(document).ready(function () { 
+    $("#tableBeneficiores").DataTable({ 
+        paging: true, 
+        pageLength: 5, 
+        autoWidth: false, 
+        processing: true, 
+        serverSide: true, 
+        ajax: { 
+            url: "/showbeneficiores", 
+        }, 
+        columns: [ 
+            { data: "id", name: "id" }, 
+            { data: "namethird", name: "namethird" }, 
+            { data: "date", name: "date" }, 
+            { data: "factura", name: "factura" }, 
+            { data: "lote", name: "lote" }, 
+            { data: "action", name: "action" }, 
+        ], 
+        order: [[0, 'DESC']], 
+        language: { 
+            processing: "Procesando...", 
+            lengthMenu: "Mostrar _MENU_ registros", 
+            zeroRecords: "No se encontraron resultados", 
+            emptyTable: "Ningún dato disponible en esta tabla", 
+            sInfo: "Mostrando del _START_ al _END_ de total _TOTAL_ registros", 
+            infoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros", 
+            infoFiltered: "(filtrado de un total de _MAX_ registros)", 
+            search: "Buscar:", 
+            paginate: { 
+                first: "Primero", 
+                last: "Último", 
+                next: "Siguiente", 
+                previous: "Anterior", 
+            }, 
+        }, 
+    }); 
+     var select2Options = { 
+        width: "100%", 
+        theme: "bootstrap-5", 
+        allowClear: true, 
+        dropdownParent: $("#modal-create-beneficiore"), 
+    }; 
+     $(".selectProvider").select2($.extend({}, select2Options, { 
+        placeholder: "Busca un proveedor", 
+    })); 
+     $(".selectPieles").select2($.extend({}, select2Options, { 
+        placeholder: "Buscar un Cliente Piel", 
+    })); 
+     $(".selectVisceras").select2($.extend({}, select2Options, { 
+        placeholder: "Buscar un Cliente Viscera", 
+    })); 
 });
+
 /*****************************************************************************************/
 const token = document
     .querySelector('meta[name="csrf-token"]')
