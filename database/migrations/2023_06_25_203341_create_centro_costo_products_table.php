86260535671ba2aaba05ef1b88c4ac31e6824cd1
eddieluis37 <eddieluis37@users.noreply.github.com>
2023-06-25 20:41:27 -0500

    Add Model and Migrate for table intermedia centro_costo_products

---
+++ database/migrations/2023_06_25_203341_create_centro_costo_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCentroCostoProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('centro_costo_products', function (Blueprint $table) {
+            $table->id();
+            
+            $table->unsignedBigInteger('centrocosto_id')->nullable();
+            $table->foreign('centrocosto_id')->references('id')->on('centro_costo');
+
+            $table->unsignedBigInteger('products_id')->nullable();
+            $table->foreign('products_id')->references('id')->on('products');
+
+            $table->decimal('stock', 18, 2)->nullable(); // valor de cantidades de unidades sea KG
+            $table->decimal('fisico', 18, 2)->nullable(); // valor de cantidades en inventario tangible real
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('centro_costo_products');
+    }
+}
