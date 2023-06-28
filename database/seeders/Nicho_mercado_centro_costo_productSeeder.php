315c4d1cdd7c30bee8fd71a9b40099f931bf550d
eddieluis37 <eddieluis37@users.noreply.github.com>
2023-06-27 22:11:15 -0500

    Add make:model Nicho_mercado_centro_costo_product  -m

---
+++ database/seeders/Nicho_mercado_centro_costo_productSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class Nicho_mercado_centro_costo_productSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+    }
+}
