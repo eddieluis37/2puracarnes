bbbb67363a54db9737f12a3981e9849d246211b3
eddieluis37 <eddieluis37@users.noreply.github.com>
2023-08-09 11:40:16 -0500

    Modelos y tablas ininiciales del modulo Workshop taller

---
+++ app/Models/workshop/Workshop.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\workshop;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Workshop extends Model
+{
+    use HasFactory;
+}
