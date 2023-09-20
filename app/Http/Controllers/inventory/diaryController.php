ee57cf353fc525e0fe9022bd01c4574feb7a9ebc
eddieluis37 <eddieluis37@users.noreply.github.com>
2023-09-19 21:13:17 -0500

    Se Agrega el Calculo de la stock ideal

--- app/Http/Controllers/inventory/diaryController.php
+++ app/Http/Controllers/inventory/diaryController.php
@@ -65,14 +65,30 @@ class diaryController extends Controller
         $data = DB::table('centro_costo_products as ccp')
             ->join('products as pro', 'pro.id', '=', 'ccp.products_id')
             ->join('categories as cat', 'pro.category_id', '=', 'cat.id')
-            ->select('cat.name as namecategoria', 'pro.name as nameproducto', 'ccp.invinicial as invinicial'
-            , 'ccp.compralote as compraLote', 'ccp.alistamiento', 'ccp.compensados as compensados', 'ccp.trasladoing as trasladoing'
-            , 'ccp.trasladosal as trasladosal', 'ccp.venta as venta', 'ccp.stock as stock', 'ccp.fisico as fisico')
+            ->select(
+                'cat.name as namecategoria',
+                'pro.name as nameproducto',
+                'ccp.invinicial as invinicial',
+                'ccp.compralote as compraLote',
+                'ccp.alistamiento',
+                'ccp.compensados as compensados',
+                'ccp.trasladoing as trasladoing',
+                'ccp.trasladosal as trasladosal',
+                'ccp.venta as venta',
+                'ccp.stock as stock',
+                'ccp.fisico as fisico'
+            )
             ->where('ccp.centrocosto_id', $centrocostoId)
             ->where('ccp.tipoinventario', 'inicial')
             ->where('pro.category_id', $categoriaId)
             ->where('pro.status', 1)
-           ->get();
+            ->get();
+
+        // Calculo de la stock ideal
+        foreach ($data as $item) {
+            $stock = $item->invinicial + $item->compraLote + $item->alistamiento + $item->trasladoing - $item->venta - $item->trasladosal;
+            $item->stock = round($stock, 2);
+        }
 
         return datatables()->of($data)
             ->addIndexColumn()
