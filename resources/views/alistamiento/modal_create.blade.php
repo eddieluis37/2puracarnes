a5847692a5c350d811d86fc974cb3ae6b2e4dec8
eddieluis37 <eddieluis37@users.noreply.github.com>
2023-10-04 10:33:31 -0500

    Primer paso para asignar fecha manual de compra y de alistamiento.

--- resources/views/alistamiento/modal_create.blade.php
+++ resources/views/alistamiento/modal_create.blade.php
@@ -1,19 +1,27 @@
 <div class="card">
 	<div class="card-body">
 		<div>
-			<input type="hidden" value="0" name="alistamientoId" id="alistamientoId" >
+			<input type="hidden" value="0" name="alistamientoId" id="alistamientoId">
 		</div>
 		<div class="row g-3">
 			<div class="col-md-6">
 				<div class="task-header">
 					<div class="form-group">
-                        <label for="" class="form-label">Categoria</label>
-					    <select class="form-control form-control-sm input " name="categoria" id="categoria" required>
-						    <option value="">Seleccione la categoria</option>
+						<label for="date1" class="form-label">Fecha</label>
+						<input type="date" class="form-control" name="fecha" id="fecha" placeholder="Last name" aria-label="Last name" value="{{date('Y-m-d')}}">
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="task-header">
+					<div class="form-group">
+						<label for="" class="form-label">Categoria</label>
+						<select class="form-control form-control-sm input " name="categoria" id="categoria" required>
+							<option value="">Seleccione la categoria</option>
 							@foreach($category as $option)
 							<option value="{{ $option['id'] }}" data="{{$option}}">{{ $option['name'] }}</option>
 							@endforeach
-					    </select>
+						</select>
 						<span class="text-danger error-message"></span>
 					</div>
 				</div>
@@ -21,13 +29,13 @@
 			<div class="col-md-6">
 				<div class="task-header">
 					<div class="form-group">
-                        <label for="" class="form-label">Centro de costo</label>
-					    <select class="form-control form-control-sm input" name="centrocosto" id="centrocosto" required>
-						    <option value="">Seleccione el centro de costo</option>
+						<label for="" class="form-label">Centro de costo</label>
+						<select class="form-control form-control-sm input" name="centrocosto" id="centrocosto" required>
+							<option value="">Seleccione el centro de costo</option>
 							@foreach($centros as $option)
 							<option value="{{ $option['id'] }}" data="{{$option}}">{{ $option['name'] }}</option>
 							@endforeach
-					    </select>
+						</select>
 						<span class="text-danger error-message"></span>
 					</div>
 				</div>
@@ -35,9 +43,9 @@
 			<div class="col-md-6">
 				<div class="task-header">
 					<div class="form-group">
-                        <label for="" class="form-label">Seleccionar corte padre</label>
-					    <select class="form-control form-control-sm input select2corte" name="selectCortePadre" id="selectCortePadre" required>
-					    </select>
+						<label for="" class="form-label">Seleccionar corte padre</label>
+						<select class="form-control form-control-sm input select2corte" name="selectCortePadre" id="selectCortePadre" required>
+						</select>
 						<span class="text-danger error-message"></span>
 					</div>
 				</div>
