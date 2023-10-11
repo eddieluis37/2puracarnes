545e5946bcb65e4bf46c11daa36addfad4a21285
eddieluis37 <eddieluis37@users.noreply.github.com>
2023-10-10 21:43:16 -0500

    Comienzo de estructura index y create modal

--- resources/views/sale/index.blade.php
+++ resources/views/sale/index.blade.php
@@ -2,10 +2,72 @@
 @section('content')
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <div class="row sales layout-top-spacing">
- <div>En construccción</div>
+ 
+<div class="col-sm-12">
+		<div class="widget widget-chart-one">
+			<div class="widget-heading">
+				<h4 class="card-title">
+					<b>Ventas | Listado </b>
+				</h4>
+				<ul class="tabs tab-pills">
+					<li>
+						<a href="javascript:void(0)" onclick="showModalcreate()" class="tabmenu bg-dark" data-toggle="modal" data-target="#modal-create-alistamiento" title="Nuevo alistamiento">Agregar</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="widget-content">
+				<div class="table-responsive">
+					<table id="tableAlistamiento" class="table table-striped mt-1">
+						<thead class="text-white" style="background: #3B3F5C">
+							<tr>
+								<th class="table-th text-white">#</th>
+								<th class="table-th text-white">Categoria</th>
+								<th class="table-th text-white ">Centro de costo</th>
+								<th class="table-th text-white ">Corte de padre</th>
+								<th class="table-th text-white ">Nuevo stock</th>
+								<th class="table-th text-white ">Inventario</th>
+								<th class="table-th text-white">Fecha</th>
+								<th class="table-th text-white text-center">Acciones</th>
+							</tr>
+						</thead>
+						<tbody>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- modal -->
+	<div class="modal fade" id="modal-create-alistamiento" aria-hidden="true" data-keyboard="false" data-backdrop="static" >
+		<div class="modal-dialog modal-xl" role="document">
+			<div class="modal-content bg-default">
+				<fieldset id="contentDisable">
+					<form action="" id="form-alistamiento">
+						<div class="modal-header">
+							<h4 class="modal-title">Crear Ventas</h4>
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span></button>
+						</div>
+				 		<div class="modal-body">
+							@include('sale.modal_create')
+						</div>
+						<div class="modal-footer">
+							<button type="button" id="btnModalClose" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+							<button type="submit" id="btnAddalistamiento" class="btn btn-primary">Aceptar</button>
+						</div>
+					</form>
+				</fieldset>
+			</div>
+			<!-- /.modal-content -->
+		</div>
+		<!-- /.modal-dialog -->
+	</div>
+	<!-- /.modal -->
+</div>
 @endsection
-
-
-
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
+@section('script')
+<script src="{{asset('rogercode/js/alistamiento/rogercode-index.js')}}"></script>
+<script src="{{asset('rogercode/js/alistamiento/rogercode-create-update.js')}}" type="module"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script> 
+@endsection
---
+++ resources/views/sale/modal_create.blade.php
@@ -0,0 +1,51 @@
+<div class="card">
+	<div class="card-body">
+		<div>
+			<input type="hidden" value="0" name="alistamientoId" id="alistamientoId">
+		</div>
+		<div class="row g-3">
+			<div class="col-md-6">
+				<div class="task-header">
+					<div class="form-group">
+						<label for="date1" class="form-label">Fecha</label>
+						<input type="date" class="form-control" name="fecha" id="fecha" placeholder="Last name" aria-label="Last name" >
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="task-header">
+					<div class="form-group">
+						<label for="" class="form-label">Categoria</label>
+						<select class="form-control form-control-sm input " name="categoria" id="categoria" required>
+							<option value="">Seleccione la categoria</option>
+							
+						</select>
+						<span class="text-danger error-message"></span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="task-header">
+					<div class="form-group">
+						<label for="" class="form-label">Centro de costo</label>
+						<select class="form-control form-control-sm input" name="centrocosto" id="centrocosto" required>
+							<option value="">Seleccione el centro de costo</option>
+							
+						</select>
+						<span class="text-danger error-message"></span>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="task-header">
+					<div class="form-group">
+						<label for="" class="form-label">Seleccionar corte padre</label>
+						<select class="form-control form-control-sm input select2corte" name="selectCortePadre" id="selectCortePadre" required>
+						</select>
+						<span class="text-danger error-message"></span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
