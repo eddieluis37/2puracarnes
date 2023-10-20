23d9afbb629c4522036363607ce71bff34bc2f90
mauriciomanjarres01 <mauriciomanjarres01@hotmail.com>
2023-10-19 22:42:54 -0500

    detalle ventas2

--- routes/web.php
+++ routes/web.php
@@ -324,6 +324,7 @@ Route::group(['middleware' => [('auth')]], function () {
     Route::get('sale{ventaId}/edit', [SaleController::class, 'edit'])->name('sale.edit');
     Route::post('sale/{ventaId}', [SaleController::class, 'update'])->name('sale.update');
     Route::get('sale/{ventaId}', [SaleController::class, 'create'])->name('sale.create');
+    Route::post('getproductosv', [SaleController::class, 'getproducts'])->name('sale.getproductos');
 
      /*****************************LISTA_DE_PRECIO******************************************/
    
