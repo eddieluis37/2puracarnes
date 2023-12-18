4830778533c1ad668675a7979517734f22594a1a
eddieluis37 <eddieluis37@users.noreply.github.com>
2023-12-18 17:28:55 -0500

    Agrega validacion para que solo active boton si valorDiferencia es <= 0

--- public/rogercode/js/caja/code-formulas.js
+++ public/rogercode/js/caja/code-formulas.js
@@ -2,7 +2,7 @@ var valorBase = formatMoneyNumber($("#base").val());
 var valorEfectivo = formatMoneyNumber($("#valor_efectivo").val());
 var valorTotal = valorBase + valorEfectivo;
 var valorIngresado = formatMoneyNumber($("#valor_real").val());
-var valorDiferencia = valorTotal - valorIngresado;
+var valorDiferencia = valorIngresado - valorTotal;
 
 document.getElementById("base").value = valorBase;
 document.getElementById("valor_efectivo").value = valorEfectivo;
@@ -10,13 +10,20 @@ document.getElementById("total").value = valorTotal;
 document.getElementById("valor_real").value = valorIngresado;
 document.getElementById("diferencia").value = valorDiferencia;
 
-$("#valor_real").on("input", function() {
-  valorIngresado = formatMoneyNumber($("#valor_real").val());
-  valorDiferencia = valorTotal - valorIngresado;
+$("#valor_real").on("input", function () {
+    valorIngresado = formatMoneyNumber($("#valor_real").val());
+    valorDiferencia = valorIngresado - valorTotal;
 
-  document.getElementById("base").value = valorBase;
-  document.getElementById("valor_efectivo").value = valorEfectivo;
-  document.getElementById("total").value = valorTotal;
-  document.getElementById("valor_real").value = valorIngresado;
-  document.getElementById("diferencia").value = valorDiferencia;
+    document.getElementById("base").value = valorBase;
+    document.getElementById("valor_efectivo").value = valorEfectivo;
+    document.getElementById("total").value = valorTotal;
+    document.getElementById("valor_real").value = valorIngresado;
+    document.getElementById("diferencia").value = valorDiferencia;
+
+    if (valorDiferencia <= 0) {
+        $("#btnGuardar").prop("disabled", false);
+    } else {
+        $("#btnGuardar").prop("disabled", true);
+    }
+    console.log(valorDiferencia);
 });
