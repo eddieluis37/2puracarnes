26dac0fc4a5569fdf333951d9cad6c2ea2f721c2
eddieluis37 <eddieluis37@users.noreply.github.com>
2024-01-25 22:53:04 -0500

    Seeder Cuentas_por_cobrar

---
+++ database/seeders/Cuentas_por_cobrarSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Cuentas_por_cobrar;
+use Illuminate\Database\Seeder;
+
+
+class Cuentas_por_cobrarSeeder extends Seeder
+{   
+    public function run()
+    {
+        //
+    }
+}
