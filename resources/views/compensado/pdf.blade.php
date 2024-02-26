8866efecb8b4c493f1d79058bbcb3a356ee8077e
eddieluis37 <eddieluis37@users.noreply.github.com>
2024-02-26 17:28:11 -0500

    Ajuste posicion vertical hoja pdf compensada

--- resources/views/compensado/pdf.blade.php
+++ resources/views/compensado/pdf.blade.php
@@ -10,7 +10,7 @@
 	<!-- cargar a través de la url del sistema -->
 
 	<link rel="stylesheet" href="{{ asset('css/custom_pdf.css') }}">
-	<link rel="stylesheet" href="{{ asset('css/custom_page.css') }}">
+	<link rel="stylesheet" href="{{ asset('css/custom_page_vertical.css') }}">
 
 	<!-- ruta física relativa OS -->
 	<!-- <link rel="stylesheet" href="{{ public_path('css/custom_pdf.css') }}">
