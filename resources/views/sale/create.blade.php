cbbcb18f1891016d905e89c2c77607155a424757
eddieluis37 <eddieluis37@users.noreply.github.com>
2024-03-11 16:43:52 -0500

    Permite editar productos en factura estado open

--- resources/views/sale/create.blade.php
+++ resources/views/sale/create.blade.php
@@ -218,7 +218,7 @@
 								<td>${{ number_format($proddetail->otro_impuesto, 0, ',', '.')}}</td>
 								<td>${{ number_format($proddetail->total, 0, ',', '.')}}</td>
 								<td class="text-center">
-									@if($status == 'true')
+									@if($status == 'false')
 									<button class="btn btn-dark fas fa-edit" name="btnEdit" data-id="{{$proddetail->id}}" title="Editar">
 									</button>
 									<button class="btn btn-dark fas fa-trash" name="btnDown" data-id="{{$proddetail->id}}" title="Borrar">
