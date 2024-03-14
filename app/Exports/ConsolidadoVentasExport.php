61e116dba1c58d8dce81f0ed726163134b9b79f5
eddieluis37 <eddieluis37@users.noreply.github.com>
2024-03-14 16:15:12 -0500

    Prueba de ajuste en consulta para compatibilidad

--- app/Exports/ConsolidadoVentasExport.php
+++ app/Exports/ConsolidadoVentasExport.php
@@ -6,13 +6,14 @@ use App\Models\Centro_costo_product;
 use App\Models\Sale;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Illuminate\Support\Facades\DB;
 
 // dd(Centro_costo_product::all());
 class ConsolidadoVentasExport implements FromCollection, WithHeadings
 {
     public function collection()
     {
-        return Sale::select('sales.id', 'sales.consecutivo', 'products.name as producto', 'sales.total', 'sales.items', 'sales.status', 'u.name as user', 'sales.created_at')
+        return Sale::select('sales.id', DB::raw('MAX(sales.consecutivo) as consecutivo'), 'products.name as producto', DB::raw('MAX(sales.total) as total'), DB::raw('MAX(sales.items) as items'), DB::raw('MAX(sales.status) as status'), DB::raw('MAX(u.name) as user'), DB::raw('MAX(sales.created_at) as created_at'))
             ->join('users as u', 'u.id', 'sales.user_id')
             ->join('sale_details as sd', 'sales.id', 'sd.sale_id')
             ->join('products', 'products.id', '=', 'sd.product_id')
@@ -20,11 +21,24 @@ class ConsolidadoVentasExport implements FromCollection, WithHeadings
             ->join('categories', 'categories.id', '=', 'products.category_id')
             ->where('sales.tipo', '0')
             ->where('sales.id', '>', '1602')
-            ->distinct('sales.id') // Filtrar ventas únicas por cada detalle de venta
-            ->groupBy('sales.id', 'products.name') // Agrupar por ID de venta y nombre de producto
+            ->groupBy('sales.id', 'products.name') // Include products.name in the GROUP BY clause
             ->get();
 
-      /*   // Reorganizar los datos para que cada fila represente un producto en una venta
+         /*   
+         Solo es compatible con version MySQL 5.7.33
+         return Sale::select('sales.id', 'sales.consecutivo', 'products.name as producto', 'sales.total', 'sales.items', 'sales.status', 'u.name as user', 'sales.created_at') 
+            ->join('users as u', 'u.id', 'sales.user_id') 
+            ->join('sale_details as sd', 'sales.id', 'sd.sale_id') 
+            ->join('products', 'products.id', '=', 'sd.product_id') 
+            ->join('meatcuts', 'meatcuts.id', '=', 'products.meatcut_id') 
+            ->join('categories', 'categories.id', '=', 'products.category_id') 
+            ->where('sales.tipo', '0') 
+            ->where('sales.id', '>', '1602') 
+            ->distinct('sales.id') // Filtrar ventas únicas por cada detalle de venta 
+            ->groupBy('sales.id', 'products.name') // Agrupar por ID de venta y nombre de producto 
+            ->get(); 
+ */
+        /*   // Reorganizar los datos para que cada fila represente un producto en una venta
         $formattedSales = collect();
         foreach ($sales as $sale) {
             $formattedSales->push([
