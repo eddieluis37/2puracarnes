d2bb4ef99991073b7fdb61d2e5d94203c66a5a0e
eddieluis37 <eddieluis37@users.noreply.github.com>
2024-03-14 20:06:12 -0500

    Homologacion de consulta para que funcione en todas las versiones de MySql

--- app/Exports/ConsolidadoVentasExport.php
+++ app/Exports/ConsolidadoVentasExport.php
@@ -13,16 +13,73 @@ class ConsolidadoVentasExport implements FromCollection, WithHeadings
 {
     public function collection()
     {
-        return Sale::select('sales.id', DB::raw('MAX(sales.consecutivo) as consecutivo'), 'products.name as producto', DB::raw('MAX(sales.total) as total'), DB::raw('MAX(sales.items) as items'), DB::raw('MAX(sales.status) as status'), DB::raw('MAX(u.name) as user'), DB::raw('MAX(sales.created_at) as created_at'))
+        return Sale::select(
+            'sales.id',
+            DB::raw('MAX(sales.consecutivo) as consecutivo'),
+            'products.name as producto',
+            DB::raw('MAX(sales.total) as total'),
+            DB::raw('MAX(sales.items) as items'),
+            DB::raw('MAX(sales.status) as status'),
+            DB::raw('MAX(u.name) as user'),
+            DB::raw('MAX(sales.created_at) as created_at'),
+            'sd.quantity',
+            'sd.price',
+            'sd.total as total_sale_detail',
+            'sales.valor_a_pagar_efectivo',
+            'sales.forma_pago_tarjeta_id',
+            'sales.forma_pago_otros_id',
+            'sales.forma_pago_credito_id',
+            'sales.valor_a_pagar_tarjeta',
+            'sales.valor_a_pagar_otros',
+            'sales.valor_a_pagar_credito',
+            'sales.total_valor_a_pagar',
+            'sales.valor_pagado',
+            'formapagos_tarjeta.nombre as forma_pago_tarjeta_nombre',
+            'formapagos_otros.nombre as forma_pago_otros_nombre'
+        )
             ->join('users as u', 'u.id', 'sales.user_id')
             ->join('sale_details as sd', 'sales.id', 'sd.sale_id')
             ->join('products', 'products.id', '=', 'sd.product_id')
             ->join('meatcuts', 'meatcuts.id', '=', 'products.meatcut_id')
             ->join('categories', 'categories.id', '=', 'products.category_id')
+            ->leftJoin('formapagos as formapagos_tarjeta', 'formapagos_tarjeta.id', '=', 'sales.forma_pago_tarjeta_id') // Unir con formapagos para forma_pago_tarjeta
+            ->leftJoin('formapagos as formapagos_otros', 'formapagos_otros.id', '=', 'sales.forma_pago_otros_id') // Unir con formapagos para forma_pago_otros
             ->where('sales.tipo', '0')
             ->where('sales.id', '>', '1602')
-            ->groupBy('sales.id', 'products.name') // Include products.name in the GROUP BY clause
+            ->groupBy('sales.id', 'products.name', 'sd.quantity', 'sd.price', 'sd.total', 'formapagos_tarjeta.nombre', 'formapagos_otros.nombre') // Incluir todos los campos en el GROUP BY
             ->get();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'CONSECUTIVO',
+            'NOMBRE PRODUCTO',
+            'TOTAL',
+            'ITEMS',
+            'STATUS',
+            'USUARIO',
+            'FECHA',
+            'CANTIDAD',
+            'PRECIO',
+            'TOTAL DETALLE',
+            'VALOR A PAGAR EFECTIVO',
+            'ID FORMA PAGO TARJETA',
+            'ID FORMA PAGO OTROS',
+            'ID FORMA PAGO CREDITO',
+            'VALOR A PAGAR TARJETA',
+            'VALOR A PAGAR OTROS',
+            'VALOR A PAGAR CREDITO',
+            'TOTAL VALOR A PAGAR',
+            'VALOR PAGADO',
+            'FORMA PAGO TARJETA NOMBRE',
+            'FORMA PAGO OTROS NOMBRE'
+        ];
+    }
+}
+
+
 
          /*   
          Solo es compatible con version MySQL 5.7.33
@@ -53,19 +110,3 @@ class ConsolidadoVentasExport implements FromCollection, WithHeadings
         }
 
         return $formattedSales; */
-    }
-
-    public function headings(): array
-    {
-        return [
-            'ID',
-            'CONSECUTIVO',
-            'PRODUCTO',
-            'TOTAL',
-            'ITEMS',
-            'STATUS',
-            'USUARIO',
-            'FECHA'
-        ];
-    }
-}
